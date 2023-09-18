50fcd7c32db2316db2780fec2e8922f7c6db45da
Luckyhudda <mr.luckyhudda@gmail.com>
2023-09-18 12:42:06 +0530

    fs module

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
-const fs = require('fs');
-const http = require('http');
-const HomePage = fs.readFileSync('home.html','utf-8');
+// const fs = require('fs');
+// const http = require('http');
+// const HomePage = fs.readFileSync('home.html','utf-8');
 
-const listner = (req,res) =>{
-    console.log(req.method);
-    console.log(req.url);
-    if(req.url == '/home'){
-        // const data = fs.readFile("home.html", "utf-8", (err, data) => {
-        //   if (err) {
-        //     console.log(err);
-        //     return;
-        //   }
-        //   console.log(data);
-        // });
+// const listner = (req,res) =>{
+//     console.log(req.method);
+//     console.log(req.url);
+//     if(req.url == '/home'){
+//         // const data = fs.readFile("home.html", "utf-8", (err, data) => {
+//         //   if (err) {
+//         //     console.log(err);
+//         //     return;
+//         //   }
+//         //   console.log(data);
+//         // });
 
-        res.write(HomePage);
-        res.end()
-    }
-    else{
+//         res.write(HomePage);
+//         res.end()
+//     }
+//     else{
 
-        res.end('file does not match')
-    }
+//         res.end('file does not match')
+//     }
 
-}
+// }
 
 
-const server = http.createServer(listner);
+// const server = http.createServer(listner);
 
-server.listen(8090,() =>{
-   console.log('file added')
-})
+// server.listen(8090,() =>{
+//    console.log('file added')
+// })
