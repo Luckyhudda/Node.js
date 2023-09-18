532f2130a1ed76f087ba9a9ae98102232a16dffb
Luckyhudda <mr.luckyhudda@gmail.com>
2023-09-18 12:18:47 +0530

    fs/ CRUD operation

--- fileSystemModule.js
+++ fileSystemModule.js
@@ -1,16 +1,12 @@
 const fs = require('fs');
 
 
-// Write file....
-const file = 'New File Added With Write File System Module'
-fs.writeFile('firstFile.js', file, (err, data) =>{
-    if(err){
-        console.log(err);
-        return;
-    }
-    console.log('File added Successfully');
-})
 
+///////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+// Write file....
+
+const file = 'some random text for a text file ......'
 fs.writeFile('file.txt',file,(err,data)=>{
     if(err){
         console.log('error', err);
@@ -36,3 +32,82 @@ fs.writeFile("data.json", JSON.stringify((finaldata)), (err, data) => {
   }
   console.log("File added Successfully");
 });
+
+
+
+///////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+//   *********** Read File *************
+
+
+fs.readFile('data.json','utf-8',(err,data)=>{
+    if(err){
+        console.log('Error',err);
+        return;
+    }
+    console.log(data);
+})
+
+
+
+///////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+//   *********** Append File *************
+
+const newData = [1,2,3,4,5,6]
+fs.appendFile('firstFile.js', JSON.stringify(newData),(err,data)=>{
+    if(err){
+        console.log('something wrong in append functionality',err);
+        return
+    }
+    if(data){
+        data = '';
+        console.log('Data Replace successfully');
+    }
+})
+
+
+///////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+
+// if you want to replace tha whole previous data .....than use this...
+
+const finalData = ['one','two', 'three','four'];
+
+
+fs.writeFile('firstFile.js',JSON.stringify(finalData),(err,data)=>{
+    if(err){
+        console.log('SomeThing went Wrong');
+        return;
+    }
+    console.log('data replace SuccessFully');
+})
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+// Delete Data....
+
+fs.writeFile('demo.txt','Demo file',(err,data)=>{
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log('New File Create for Demo');
+})
+
+// now delete file
+
+fs.unlink('demo.txt',(err,data)=>{
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log('File delete SuccessFully....');
+})
--- firstFile.js
+++ firstFile.js
@@ -1,1 +1,1 @@
-New File Added With Write File System Module
+["one","two","three","four"]
